ded06faa986727e972a7d740cf78b59b31db5d4f
Yu-Cheng Lee <b12901144@ntu.edu.tw>
2026-06-25 15:28:20 +0800

    chore: remove and ignore unused scripts to keep repo clean

--- finetune/check_bos.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoProcessor
-processor = AutoProcessor.from_pretrained("google/gemma-4-E4B-it")
-messages = [[{"role": "user", "content": [{"type": "text", "text": "hello"}]}]]
-text = processor.apply_chat_template(messages, tokenize=False)
-print("Template text:", repr(text[0]))
-encoded = processor(text=text, return_tensors="pt")
-print("Tokens:", encoded["input_ids"][0][:10])
-print("Token strings:", processor.batch_decode(encoded["input_ids"][0][:10]))
